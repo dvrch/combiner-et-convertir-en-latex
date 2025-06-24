3c65d6c29f8edbde0d18162fd30956b6391bc550
Victorien DJONTSO <121665000+dvrch@users.noreply.github.com>
2025-06-24 02:10:34 +0200

    suppession

--- src/settings.ts
+++
@@ -1,9 +0,0 @@
-// ARCHIVÉ : Les paramètres sont désormais gérés dans src/stores/settings.store.ts (store Svelte)
-
-export interface PluginSettings {
-    useHiddenEmbeds: boolean;
-}
-
-export const DEFAULT_SETTINGS: PluginSettings = {
-    useHiddenEmbeds: true,
-}; 
